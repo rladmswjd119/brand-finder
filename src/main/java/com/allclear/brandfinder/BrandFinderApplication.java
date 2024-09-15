164883bc4f7d51c575b14606c53bb06f23ffb452
Eunjung <fkznsha23@gmail.com>
2024-09-15 12:14:11 +0900

    Test

---
+++ src/main/java/com/allclear/brandfinder/BrandFinderApplication.java
@@ -0,0 +1,14 @@
+package com.allclear.brandfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandFinderApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BrandFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allclear/brandfinder/BrandFinderApplicationTests.java
@@ -0,0 +1,14 @@
+package com.allclear.brandfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
