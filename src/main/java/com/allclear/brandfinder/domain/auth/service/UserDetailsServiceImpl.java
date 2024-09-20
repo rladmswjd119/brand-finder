b84f7b4e97e21925f99536bc217a0eba2a2af8d6
Eunjung <fkznsha23@gmail.com>
2024-09-20 20:46:39 +0900

    [Test] 브랜드별 상품 조회 통합 테스트 작성

--- src/main/java/com/allclear/brandfinder/domain/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/com/allclear/brandfinder/domain/auth/service/UserDetailsServiceImpl.java
@@ -1,15 +1,21 @@
 package com.allclear.brandfinder.domain.auth.service;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.allclear.brandfinder.domain.auth.dto.UserDetailsImpl;
+import com.allclear.brandfinder.domain.users.dto.JoinForm;
 import com.allclear.brandfinder.domain.users.entity.User;
 import com.allclear.brandfinder.domain.users.repository.UserRepository;
+import com.allclear.brandfinder.global.exception.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,12 +25,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = Optional.of(userRepository.findByUsername(username))
-                .orElseThrow(() -> new RuntimeException("가입되지 않은 계정입니다."));
+                .orElseThrow(() -> new NullPointerException(ErrorCode.ACCOUNT_NOT_FOUND.getMessage()));
 
         log.info("{}의 등급은 {} 입니다.", user.getUsername(), user.getRank().getName());
 
--- src/test/java/com/allclear/brandfinder/products/ProductControllerTest.java
+++ src/test/java/com/allclear/brandfinder/products/ProductControllerTest.java
@@ -2,20 +2,24 @@ package com.allclear.brandfinder.products;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.data.domain.Page;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
+import com.allclear.brandfinder.domain.auth.util.AccessTokenUtil;
 import com.allclear.brandfinder.domain.products.dto.ProductLoginResponse;
 import com.allclear.brandfinder.domain.products.dto.ProductNoLoginResponse;
-import com.allclear.brandfinder.domain.products.dto.ProductResponse;
+import com.allclear.brandfinder.domain.users.dto.JoinForm;
 import com.allclear.brandfinder.global.response.SuccessResponse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,12 +27,60 @@ public class ProductControllerTest {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
+    @Autowired
+    private AccessTokenUtil accessTokenUtil;
+
+    private JoinForm form;
+
+    @BeforeEach
+    public void setUp(){
+        form = JoinForm.builder()
+                .username("username")
+                .password("password1234!!")
+                .email("rladmswjd23@naver.com")
+                .birth(LocalDate.of(1997,1,19))
+                .build();
+
+        HttpEntity<JoinForm> entity = new HttpEntity<>(form);
+        ResponseEntity<String> result = testRestTemplate.exchange("/api/users/join", HttpMethod.POST, entity, String.class);
+    }
 
     @Test
-    public void 상품목록_로그인_없는_조회_테스트() {
+    public void 상품목록_로그인_없이_조회_테스트() {
+
+        ResponseEntity<SuccessResponse<List<ProductLoginResponse>>> responseEntity
+                = testRestTemplate.exchange("/api/products/", HttpMethod.GET, null,
+                new ParameterizedTypeReference<>() {});
+
+
+        List<ProductLoginResponse> result = responseEntity.getBody().getData();
+
+        assertThat(result.size()).isEqualTo(10);
+    }
+
+    @Test
+    public void 상품목록_로그인_조회_테스트() {
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        String token = accessTokenUtil.createAccessToken(form.getUsername());
+        httpHeaders.add("Authorization", token);
+        HttpEntity<String> entity = new HttpEntity<>(null, httpHeaders);
 
         ResponseEntity<SuccessResponse<List<ProductNoLoginResponse>>> responseEntity
-                = testRestTemplate.exchange("/api/products/", HttpMethod.GET, null,
+                = testRestTemplate.exchange("/api/products/", HttpMethod.GET, entity,
+                new ParameterizedTypeReference<>() {});
+
+
+        List<ProductNoLoginResponse> result = responseEntity.getBody().getData();
+
+        assertThat(result.size()).isEqualTo(10);
+    }
+
+    @Test
+    public void 브랜드별_상품목록_조회_테스트() {
+
+        ResponseEntity<SuccessResponse<List<ProductNoLoginResponse>>> responseEntity
+                = testRestTemplate.exchange("/api/products/" + 4, HttpMethod.GET, null,
                 new ParameterizedTypeReference<>() {});
 
 
