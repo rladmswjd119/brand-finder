4609fc0a893b204f28a2d1a0d8579ef35a3b3ada
Eunjung <fkznsha23@gmail.com>
2024-09-20 09:56:53 +0900

    [Test] 상품 목록 조회 통합 테스트 작성

--- src/test/java/com/allclear/brandfinder/products/ProductControllerTest.java
+++ src/test/java/com/allclear/brandfinder/products/ProductControllerTest.java
@@ -1,8 +1,34 @@
 package com.allclear.brandfinder.products;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import com.allclear.brandfinder.domain.products.dto.ProductLoginResponse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductControllerTest {
+
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void 상품목록_로그인_조회_테스트() {
+
+        ResponseEntity<List<ProductLoginResponse>> responseEntity
+                = testRestTemplate.exchange("/api/produts", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<ProductLoginResponse>>() {});
+
+        List<ProductLoginResponse> result = responseEntity.getBody();
+
+        assertThat(result.size()).isEqualTo(10);
+    }
 
 }
